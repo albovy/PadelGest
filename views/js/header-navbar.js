5e78be0601a67c2b628b2cd7f51d1a4112c38588
Jose <jdvazquezdev@gmail.com>
2019-11-12 19:47:26 +0100

    Add index view updates

---
+++ views/js/header-navbar.js
@@ -0,0 +1,26 @@
+function topnavDisplay() {
+  let topnav = document.getElementById("myTopnav");
+  if (topnav.className === "topnav") {
+    topnav.className += " responsive";
+  } else {
+    topnav.className = "topnav";
+  }
+} 
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+let header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+let sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
--- views/js/topnav.js
+++
@@ -1,8 +0,0 @@
-function topnavDisplay() {
-  let topnav = document.getElementById("myTopnav");
-  if (topnav.className === "topnav") {
-    topnav.className += " responsive";
-  } else {
-    topnav.className = "topnav";
-  }
-} 
