66213657fef86d64ab8bc3af1183f3b9d047d4c5
José Domínguez Vázquez <jdvazquezdev@gmail.com>
2020-01-02 16:37:37 +0100

    Minor Fix: Add new control statements to solve bugs in header and navbar

--- views/js/header-navbar.js
+++ views/js/header-navbar.js
@@ -1,5 +1,5 @@
 addEventListener('load', function () {
-  
+
   topnavDisplay();
 
   // When the user scrolls the page, execute stickyHeader
@@ -11,24 +11,23 @@ addEventListener('load', function () {
   let stickyNavbar = document.querySelectorAll("sticky-nav");
 
   // Get the offset position of the navbar
-  let sticky = header.offsetTop;
+  let sticky = header.offsetTop + navbar.offsetTop;
 
 
   // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
   function stickyHeader() {
-    if (window.pageYOffset > sticky) {
-      header.classList.add("sticky");
-     
-      // stickyNavbar.forEach(element => {
-      //  stickyNavbar.style.top = header.clientHeight + navDivElement.getBoundingClientRect().top + 'px';
-      // });
-      
-      navbar.classList.add("sticky-nav");
-      navbar.style.top = header.scrollHeight + header.getBoundingClientRect().top + 'px'
-     
-    } else {
-      header.classList.remove("sticky");
-      navbar.classList.remove("sticky-nav");
+    if (document.documentElement.scrollHeight > 1200) {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+        if (window.innerWidth > 950) {
+          navbar.style.top = header.scrollHeight + header.getBoundingClientRect().top + 'px';
+          navbar.classList.add("sticky-nav");
+        }
+
+      } else {
+        header.classList.remove("sticky");
+        navbar.classList.remove("sticky-nav");
+      }
     }
   }
 }, false);
