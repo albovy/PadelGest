8913a0e575f23be87bcda3e70df2a4918c64d2f5
Jose <jdvazquezdev@gmail.com>
2020-01-11 17:56:05 +0100

    Update: Add new style to show posts
    Also solve  minor fix with sticky header in showcurrent blogs view

--- views/js/header-navbar.js
+++ views/js/header-navbar.js
@@ -1,32 +1,35 @@
 addEventListener('load', function () {
 
   topnavDisplay();
-
+  // Check if is a showcurrent of blog post to solve a error with sticky header
+  // It isnt a solution, its a temporal patch
+ if (!window.location.pathname.startsWith('/blog/show/')) {
   // When the user scrolls the page, execute stickyHeader
-  window.onscroll = function () { stickyHeader() };
-
-  // Get the header
-  let header = document.getElementById("myHeader");
-  let navbar = document.getElementById("myTopnav");
-  let stickyNavbar = document.querySelectorAll("sticky-nav");
-
-  // Get the offset position of the navbar
-  let sticky = header.offsetTop + navbar.offsetTop;
+  window.addEventListener('scroll', function () { stickyHeader(), false });
+ 
+    // Get the header
+    let header = document.getElementById("myHeader");
+    let navbar = document.getElementById("myTopnav");
 
 
-  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-  function stickyHeader() {
-    if (document.documentElement.scrollHeight > 1200) {
-      if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-        if (window.innerWidth > 950) {
-          navbar.style.top = header.scrollHeight + header.getBoundingClientRect().top + 'px';
-          navbar.classList.add("sticky-nav");
+    // Get the offset position of the navbar
+    let sticky = header.offsetTop + navbar.offsetTop;
+
+
+    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function stickyHeader() {
+      if (document.documentElement.scrollHeight > 1200) {
+        if (window.pageYOffset > sticky) {
+          header.classList.add("sticky");
+          if (window.innerWidth > 950) {
+            navbar.style.top = header.scrollHeight + header.getBoundingClientRect().top + 'px';
+            navbar.classList.add("sticky-nav");
+          }
+
+        } else {
+          header.classList.remove("sticky");
+          navbar.classList.remove("sticky-nav");
         }
-
-      } else {
-        header.classList.remove("sticky");
-        navbar.classList.remove("sticky-nav");
       }
     }
   }
