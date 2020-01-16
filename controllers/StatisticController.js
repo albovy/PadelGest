45ad0f9e8674d6f4e988c5e52d09b7ea7c53fb2c
Rodrigo Currás <rcferradas@esei.uvigo.es>
2020-01-16 23:24:51 +0100

    Add: Add dummy chart for testing

--- controllers/StatisticController.js
+++ controllers/StatisticController.js
@@ -9,10 +9,35 @@ class StatisticController{
     constructor(){}
 
     //no estoy muy seguro de esta cabecera
-    show(res){
+    show(req,res){
         console.log("Show Statistics");
-
+        var barChartExample = this.generateBars("bar-chart-example");
+        console.log("A renderizar graficas");
         res.render("/statistic.twig");  //anhadir nuevos parametros necesarios para la vista
+    }
+
+    generateBars(canvasId){
+        // Bar chart 
+        new Chart(document.getElementById(canvasId), {
+            type: 'bar',
+            data: {
+            labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+            datasets: [
+                {
+                label: "Population (millions)",
+                backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+                data: [2478,5267,734,784,433]
+                }
+            ]
+            },
+            options: {
+            legend: { display: false },
+            title: {
+                display: true,
+                text: 'Predicted world population (millions) in 2050'
+            }
+            }
+        });
     }
 }
 
