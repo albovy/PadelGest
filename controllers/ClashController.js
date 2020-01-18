27bb2fc98d3d9874a437aabc8e79eefa0766de90
Brais <bfalonso@esei.uvigo.es>
2020-01-18 23:39:03 +0100

    Add: Contact method for clashes

--- controllers/ClashController.js
+++ controllers/ClashController.js
@@ -3,6 +3,8 @@ const CompetitionModel = require("../models/CompetitionModel");
 const CourtModel = require("../models/CourtModel");
 const BookModel = require("../models/BookModel");
 const UserModel = require("../models/UserModel");
+const nodemailer = require('nodemailer');
+const Email = require('email-templates');
 
 class ClashController {
   constructor() {}
@@ -12,6 +14,62 @@ class ClashController {
     const myClash = await ClashModel.findByUser(req.user.id);
 
     res.render("clash/showAll", { clashes: myClash });
+  }
+
+  async contact(req,res){
+    let lider1 = await UserModel.findOne(req.params.user1_id);
+    let lider2 = await UserModel.findOne(req.params.user3_id);
+
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: 'padelgest2020@gmail.com',
+        pass: 'padel20gest'
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
+
+    const email = new Email({
+      message: {
+        from: 'padelgest2020@gmail.com'
+      },
+      // uncomment below to send emails in development/test env:
+      send: true,
+      transport: transporter,
+      preview: false
+    });
+     
+    email.send({
+        template: 'contact',
+        message: {
+          to: lider1.email
+        },
+        locals: {
+          username: lider1.login,
+          lider1: lider1.email,
+          lider2: lider2.email
+        }
+      })
+      .then(console.log)
+      .catch(console.error);
+
+      email.send({
+        template: 'contact',
+        message: {
+          to: lider2.email
+        },
+        locals: {
+          username: lider2.login,
+          lider1: lider1.email,
+          lider2: lider2.email
+        }
+      })
+      .then(console.log)
+      .catch(console.error);
+
+       res.redirect('/tournament');
   }
 
   async showTournament(req, res) {
--- routes/clash.js
+++ routes/clash.js
@@ -6,6 +6,7 @@ const clashController = require("../controllers/ClashController");
 
 router.get("/", permit("ATHLETE","COACH"), clashController.show);
 router.get("/tournament/:id",permit("ATHLETE","COACH"),clashController.showTournament);    
+router.get("/contact/:user1_id/:user3_id",permit("ATHLETE","COACH"),clashController.contact);
 
 
 module.exports = router;
